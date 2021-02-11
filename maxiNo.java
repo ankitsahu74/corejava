dbe758b3d6ed1c538c91b64f406beba82c0edf7b
Ankitsahoo <sahu74824@gmail.com>
2021-02-11 23:01:19 +0530

    sum

---
+++ maxiNo.java
@@ -0,0 +1,15 @@
+package javabasic;
+
+public class maxiNo {
+	public static void main(String[] args) {
+		int a=50;
+		int b=20;
+		if(a>b) {
+			System.out.println("max no"+a);
+		}else {
+				System.out.println("max no"+b);
+		  
+		}
+	}
+
+}
